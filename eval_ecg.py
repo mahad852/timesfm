14a78907e6762f06bef0242d1bdfe269585de243
Mahad <mahad852@gmail.com>
2024-06-09 01:47:11 +0500

    added logs

--- eval_ecg.py
+++ eval_ecg.py
@@ -3,13 +3,14 @@ from datasets.ecg_mit import ECG_MIT
 import random
 import timesfm
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+import os
 
 context_len = 512
 pred_len = 64
 ecg_dataset = ECG_MIT(context_len=context_len, pred_len=pred_len, data_path="/home/user/MIT-BIH.npz")
 
 
-max_len = 286000
+max_len = 260000
 batch_size = 64
 
 def single_loader(dataset: ECG_MIT, indices: list[int]):
@@ -31,12 +32,6 @@ def batch_loader(dataset: ECG_MIT, indices: list[int], batch_size: int):
 
 
 indices = random.sample(range(len(ecg_dataset)), max_len)
-# print("Ecg dataset length:", len(ecg_dataset))
-
-# print(ecg_dataset[1298433][0].shape,  ecg_dataset[1298433][1].shape)
-
-# for i, (x, y) in enumerate(batch_loader(ecg_dataset, indices, batch_size)):
-#     print(i, "|", len(x), len(y))
 
 tfm = timesfm.TimesFm(
     context_len=context_len,
@@ -49,40 +44,55 @@ tfm = timesfm.TimesFm(
 )
 tfm.load_from_checkpoint(repo_id="google/timesfm-1.0-200m")
 
-forecast_input = [
-    np.sin(np.linspace(0, 20, 100)),
-    np.sin(np.linspace(0, 20, 200)),
-]
-frequency_input = [0, 1, 2]
-
-
 mses = []
 maes = []
 rmses = []
 
-for i, (x, y) in enumerate(batch_loader(ecg_dataset, indices, batch_size)):
-    point_forecast, experimental_quantile_forecast = tfm.forecast(
-        x,
-        # freq=frequency_input,
-    )
+mse_by_pred_len = {}
+rmse_by_pred_len = {}
+mae_by_pred_len = {}
 
-    mse = mean_squared_error(np.array(y), np.array(point_forecast))
+total = 0
+
+for p_len in range(1, pred_len + 1):
+    mse_by_pred_len[p_len] = 0.0
+    rmse_by_pred_len[p_len] = 0.0
+    mae_by_pred_len[p_len] = 0.0
+
+for i, (x, y) in enumerate(batch_loader(ecg_dataset, indices, batch_size)):
+    point_forecast, experimental_quantile_forecast = tfm.forecast(x)
+
+    point_forecast = np.array(point_forecast)
+    y = np.array(y)
+
+    mse = mean_squared_error(y, point_forecast)
     rmse = np.sqrt(mse)
-    mae = mean_absolute_error(np.array(y), np.array(point_forecast))
+    mae = mean_absolute_error(y, point_forecast)
 
     mses.append(mse)
     rmses.append(rmse)
     maes.append(mae)
 
+    total += 1
+
+    for p_len in range(1, pred_len + 1):
+        mse_by_pred_len[p_len] += mean_squared_error(y[:, :p_len], point_forecast[:, :p_len])
+        mse_by_pred_len[p_len] += np.sqrt(mean_squared_error(y[:, :p_len], point_forecast[:, :p_len]))
+        mae_by_pred_len[p_len] += mean_absolute_error(y[:, :p_len], point_forecast[:, :p_len])
+
     if i % 20 == 0:
         print(f"iteraition: {i} | MSE: {mse} RMSE: {rmse} MAE: {mae}")
 
-# point_forecast, experimental_quantile_forecast = tfm.forecast(
-#     forecast_input,
-#     freq=frequency_input,
-# )
-
-# print(point_forecast, experimental_quantile_forecast)
-# print(point_forecast[0].shape)
-
 print(f"MSE: {np.average(mses)} RMSE: {np.average(rmses)} MAE: {np.average(maes)}")
+
+for p_len in range(1, pred_len + 1):
+    mse_by_pred_len[p_len] /= total
+    rmse_by_pred_len[p_len] /= total
+    mae_by_pred_len[p_len] /= total
+
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+
+with open(os.path.join("logs", f"TimesFM_{context_len}_{pred_len}.txt"), "w") as f:
+    for p_len in range(1, pred_len + 1):
+        f.write(f"pred_len: {p_len}; MSE: {mse_by_pred_len[p_len]}; RMSE: {rmse_by_pred_len[p_len]}; MAE: {mae_by_pred_len[p_len]}\n")
